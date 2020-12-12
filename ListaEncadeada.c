52d81d5ab798c3704d99da494de1da8f158bff6a
Gustavo Maltez <gustavo.maltez@hotmail.com>
2020-12-12 09:45:40 -0300

    added add function base code

--- ListaEncadeada.c
+++ ListaEncadeada.c
@@ -6,11 +6,25 @@ typedef struct{
     struct Node *prox;
 }Node;
 
+Node *firstPosition = NULL;
+int listLength = 0;
+
+void addNewValue(int value){
+    if(listLength == 0){
+
+    }else{
+        
+    }
+}
+
 int main()
 {
     Node *MyNewNode = malloc(sizeof(Node));
     MyNewNode->value = 123;
+    MyNewNode->prox = NULL;
     printf("Value is: %d\n",MyNewNode->value);
+
+    firstPosition = novo;
 
     return 0;
 }
